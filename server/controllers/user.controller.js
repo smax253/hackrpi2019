f0459f41f50aec3f6a33857fef52c58571ac42a9
Lachlan McCarty <lachlan@lachm.com>
2019-03-17 01:36:48 -0400

    snap kit auth partially, trip advisor endpoint

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,13 +1,9 @@
-const bcrypt = require('bcrypt');
 const Joi = require('joi');
 const User = require('../models/user.model');
 
 const userSchema = Joi.object({
-  fullname: Joi.string().required(),
-  email: Joi.string().email(),
-  mobileNumber: Joi.string().regex(/^[1-9][0-9]{9}$/),
-  password: Joi.string().required(),
-  repeatPassword: Joi.string().required().valid(Joi.ref('password'))
+  displayName: Joi.string().required(),
+  snapId: Joi.string().required(),
 })
 
 
@@ -17,7 +13,5 @@ module.exports = {
 
 async function insert(user) {
   user = await Joi.validate(user, userSchema, { abortEarly: false });
-  user.hashedPassword = bcrypt.hashSync(user.password, 10);
-  delete user.password;
   return await new User(user).save();
 }
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,20 +1,14 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  fullname: {
+  displayName: {
     type: String,
     required: true
   },
-  email: {
+  snapId: {
     type: String,
     required: true,
     unique: true,
-    // Regexp to validate emails with more strict rules as added in tests/users.js which also conforms mostly with RFC2822 guide lines
-    match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email'],
-  },
-  hashedPassword: {
-    type: String,
-    required: true
   },
   createdAt: {
     type: Date,
